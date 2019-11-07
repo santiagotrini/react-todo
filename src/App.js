a5bf48d22de9a55cce5abf45d1e3c41fecf6dc2f
Santiago Trini <santiagotrini@gmail.com>
2019-11-07 12:52:46 -0300

    agrega comentarios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from 'react';  // importo React y useState (hook)
+import './App.css';  // un CSS para darle un poco de onda
 
 // componente principal (la App)
 function App() {
 
   // state con hooks
   // el argumento de useState es el estado inicial
+  // el estado es el array de tareas, cada tarea es un objeto
   const [todos, setTodos] = useState([
-    { 
-      text: 'Pasear al perro', 
+    {
+      text: 'Pasear al perro',
       isCompleted: false
     },
-    { 
-      text: 'Lavar la ropa', 
+    {
+      text: 'Lavar la ropa',
       isCompleted: false
     },
-    { 
-      text: 'Hacer las compras', 
+    {
+      text: 'Hacer las compras',
       isCompleted: false
     }
   ]);
-  
+
   // funciones que modifican el estado de la App
-  // agregar una tarea 
+  // hay que llamar setTodos con el nuevo estado al final de cada funcion
+  // agregar una tarea
+  // uso spread y destructuring para armar el array del nuevo estado
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
   // borrar una tarea
+  // splice(index, 1) borra un elemento de un array a partir de index
   const removeTodo = index => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
@@ -41,20 +45,24 @@ function App() {
   };
 
   // lo que se muestra (renderiza) como JSX
+  // son dos divs anidados, el div de adentro contiene la lista de tareas y el formulario
+  // que son los otros dos componentes (Todo y TodoForm)
+  // uso la funcion map de arrays para crear una lista de componentes
+  // el otro componente (formulario) despues de la lista de tareas
+  // le paso la funcion addTodo() como props
   return (
     <div className='app'>
       <div className='todo-list'>
         {todos.map((todo, index) => (
           <Todo // aca uso otro componente que esta definido mas abajo
-            key={index} 
-            index={index} 
-            todo={todo} 
+            key={index}
+            index={index}
+            todo={todo}
             completeTodo={completeTodo}
             removeTodo={removeTodo}
-          /> // este componente no tiene estado, asi que recibe su estado de App como props 
-          
-            
+          /> // este componente no tiene estado, asi que recibe su estado de App como props
         ))}
+
         <TodoForm addTodo={addTodo} />
       </div>
     </div>
@@ -62,6 +70,9 @@ function App() {
 }
 
 // componente que representa una tarea (Todo)
+// recibe props de App, un objeto todo (una tarea), el indice de map y dos funciones
+// uso inline style para tachar las tareas con el operador ternario
+// los dos botones usan las funciones que recibe de App
 function Todo({ todo, index, completeTodo, removeTodo }) {
   return (
     <div className='todo' style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
@@ -75,20 +86,21 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
 }
 
 // componente que representa el formulario de agregar tarea (TodoForm)
-// tiene estado (el valor del input)
+// tiene estado (el valor del input del formulario)
 function TodoForm({ addTodo }) {
   const [value, setValue] = useState('');
-
+  // listener para cuando submiteamos el formulario (presionando enter porque no hay boton)
   const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue('');
+    e.preventDefault();  // para no enviar el form y refrescar la pagina
+    if (!value) return;  // si no hay valor en el input no agrega nada
+    addTodo(value);      // cambiamos el estado de App
+    setValue('');        // hacemos clear del input
   };
-  
+  // un form re simple con un solo input
+  // el onChange listener actualiza el estado de este componente (TodoForm)
   return (
     <form onSubmit={handleSubmit}>
-      <input 
+      <input
         type='text'
         className='input'
         value={value}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+// esto pega mi componente principal (App) en el index.html
 ReactDOM.render(<App />, document.getElementById('root'));
